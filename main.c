82115258aad64aae6ddc94f661133e643d58e41a
adinab <asbruce@mail.yu.edu>
2020-12-27 17:05:41 -0500

    Initial commit

--- main.c
+++ main.c
@@ -32,6 +32,7 @@ int main(int argc, char *argv[]) {
             printf("did not process FSM file correctly\n");
             return (-1);
         }
+
         printf("starting fsm testing\n");
         if (test_fsmProcess()!=0){
             printf("test failed\n");
@@ -56,26 +57,31 @@ int main(int argc, char *argv[]) {
 
 int fsmDef(FILE *def) {
     int c = 0;
+    // assigns 3 variables to structure
     while (fscanf(def, "%d:%c>%d", &arr[c].curState, &arr[c].input, &arr[c].nextState) != EOF) {
         c += 1;
     }
-    return (c);
+    return (c); //number of transitions
 }
 int fsmProcess(int cur2, int b,struct States FSM[b], char input){
     int k;
+    //b is the number of transitions in the FSM
     for (k = 0; k < b; k++) {
+        //loops through the FSM to find the correct state to transition to with the given input
         if (FSM[k].curState == cur2 && (input == FSM[k].input)) {
             printf("input %c transitions from state %d to state %d \n", input, cur2, FSM[k].nextState);
             cur2 = FSM[k].nextState;
             break;
         }
     }
+    //returns state that it transitioned to
     return cur2;
 }
 int fsmInputs(int a, FILE *inp) {
     char line2[256];
     int j = 0;
     int cur = 0;
+    // loops through inputs in the file changing states using the fsmProcess function
     while(fscanf(inp,"%c",line2)!=EOF){
         cur=fsmProcess(cur, a, arr, line2[0]);
         j++;
@@ -84,6 +90,7 @@ int fsmInputs(int a, FILE *inp) {
     return (0);
 }
 int test_fsmProcess(){
+    //creates mini fsm and tests that fsmProcess processes inputs correctly with that fsm
     struct States test[3];
     test[0].input='a';
     test[0].curState=0;
